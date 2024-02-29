a284a0a34220bd3de708f23f182478d2519f531d
ss-Akuma <amindashti2019@gmail.com>
2024-03-01 01:46:46 +0330

    handling type errors and adding switch

--- calculator.cpp
+++ calculator.cpp
@@ -1,48 +1,84 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int main(){
 
-    int num_1, num_2;
-    string oprt;
+    int num_2, num_1;
+    char oprt;
     int result;
 
     cout << "### Welcome to Simple Calculator ###" << endl;
-    cout << "Enter first number: " << endl;
-    cin >> num_1;
-    cout << "Enter second number: " << endl;
-    cin >> num_2;
 
-    cout << "Enter Operation (+ , - , * , /): " << endl;
-    cin >> oprt;
-
-    while (true)
-    {
-        if(oprt == "+"){
-            result = num_1 + num_2;
-            cout << result << endl;
-            break;
-        }
-        else if (oprt == "-"){
-            result = num_1 - num_2;
-            cout << result << endl;
-            break;
-        }else if (oprt == "*"){
-            result = num_1 * num_2;
-            cout << result << endl;
-            break;
-        }else if (oprt == "/"){
-            result = num_1 / num_2;
-            cout << result << endl;
-            break;
-        }else if (oprt == "Exit")
-        {
-            break;
-        }else{
-            cout << "please choose between these operations! or simply type Exit" << endl;
-        }
+    while (true){ 
         
+        try
+        {
+            cout << "Enter first number: " << endl;
+            cin >> num_1;
+
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                throw runtime_error ("invalid first number input!");
+            }
+
+            cout << "Enter Operation (+ , - , * , /) or 'e' to exit: " << endl;
+            cin >> oprt;
+
+            if (oprt == 'e'){
+                break;
+            }
+
+            cout << "Enter second number: " << endl;
+            cin >> num_2;
+
+            if (cin.fail()){
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                throw runtime_error ("invalid second number input!");
+            }
+
+            switch (oprt){
+                case '+':
+                    result = num_1 + num_2;
+                    cout << result << endl;
+                    break;
+                
+                case '-':
+                    result = num_1 - num_2;
+                    cout << result << endl;
+                    break;
+
+                case '*':
+                    result = num_1 * num_2;
+                    cout << result << endl;
+                    break;
+
+                case '/':
+                    if (num_2 != 0)
+                    {
+                    result = num_1 / num_2;
+                    cout << result << endl;
+                    
+                    }else{
+                        cout << "division by zero!";
+                    }
+                    break; 
+                    
+                default:
+                    cout << "invalid operation!" << endl;
+                    break;
+            }
+            
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        cout << endl;
     }
-    
+       
     return 0;
 }
